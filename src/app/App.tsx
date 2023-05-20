90212075103552dee0b954a46c5e995af2e98626
Vladimir Yakovlev <v.yakovlev@mindbots.ru>
2023-05-20 21:24:53 +0300

    Add loader and 404 page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,14 +4,16 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
 import 'app/styles/index.scss';
+import { Navbar } from 'widgets/Navbar';
 
 const App = (): JSX.Element => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback="">
-        <div className="content-page">
+      <Suspense fallback=''>
+        <Navbar />
+        <div className='content-page'>
           <Sidebar />
           <AppRouter />
         </div>
